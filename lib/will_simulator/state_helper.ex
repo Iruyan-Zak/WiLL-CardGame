8f84ecd08c508fd7abead42c355ae43707441996
Iruyan_Zak <yuri.kazuna@gmail.com>
2018-03-30 21:37:58 +0900

    `*select` functions can handle :invalid input and :cancel.

--- lib/will_simulator/state_helper.ex
+++ lib/will_simulator/state_helper.ex
@@ -1,30 +1,46 @@
 defmodule WillSimulator.StateHelper do
   @input_source Application.get_env(:will_simulator, :input_source)
 
-  def conditional_select(index_generator \\ @input_source, enum, predicate) do
-    picked = with [picked | _] <-
-      index_generator.()
+  def create_input(generator) do
+    input = with [atom | vals] <-
+      generator.()
       |> Stream.uniq
-      |> Stream.take_while(&(&1 !== :end))
-      |> Stream.map(&(Enum.at(enum, &1)))
-      |> Stream.reject(&(&1 === nil))
-      |> Stream.scan([], &[&1 | &2])
-      |> Stream.take_while(&(predicate.(&1)))
-      |> Enum.reverse
+      |> Stream.scan([],&[&1 | &2])
+      |> Stream.drop_while(fn [head | _] -> not(head === :end) end)
+      |> Stream.take(1)
+      |> Stream.map(fn list -> list |> Enum.take_while(&(&1 !== :reset)) end)
+      |> Enum.at(0)
     do
-      picked
+      {atom, vals}
     end
-    {picked, Enum.to_list(enum) -- picked}
+
+    case input do
+      {:end, vals} -> {:ok, vals |> Enum.filter(&(not is_atom(&1))) |> Enum.reverse}
+      _ -> :error
+    end
+  end
+
+  def conditional_select(index_generator \\ @input_source, enum, predicate) do
+    candidate = Enum.to_list(enum)
+    selected = with {:ok, vals} <- create_input(index_generator)
+    do
+      vals
+      |> Stream.map(&(Enum.at(enum, &1)))
+      |> Enum.reject(&(&1 === nil))
+    end
+
+    case selected do
+      :invalid -> :error
+      _vals ->
+        if predicate.(selected) do
+          {selected, candidate -- selected}
+        else
+          :error
+        end
+    end
   end
 
   def select(index_generator \\ @input_source, enum, count \\ 1) do
-    picked =
-      index_generator.()
-      |> Stream.uniq
-      |> Stream.map(&(Enum.at(enum, &1)))
-      |> Stream.reject(&(&1 === nil))
-      |> Enum.take(count)
-
-    {picked, Enum.to_list(enum) -- picked}
+    conditional_select(index_generator, enum, &(length(&1) === count))
   end
 end
--- test/will_simulator_test.exs
+++ test/will_simulator_test.exs
@@ -21,15 +21,19 @@ defmodule WillSimulatorTest do
   test "conditional select" do
     selector = &(conditional_select(&1, 0..9,
           fn(list) -> Enum.sum(list) < 20 end))
-    {picks, rests} = selector.(fn -> 0..3 end)
+    {picks, rests} = selector.(fn -> [0,1,2,3,:end] end)
     assert length(picks) == 4
     assert length(rests) == 6
     {picks, _rests} = selector.(fn -> [7,:end,8,9] end)
     assert length(picks) == 1
+    result = selector.(fn -> [:cancel] end)
+    assert result === :cancel
+    result = selector.(fn -> [0,1,2,3] end)
+    assert result === :invalid
   end
 
   test "select" do
-    {picks, rests} = select(fn -> [0,0,2,3] end, 4..8, 2)
+    {picks, rests} = select(fn -> [0,2,:end] end, 4..8, 2)
     assert 6 in picks
     assert not(7 in picks)
     assert length(picks) == 2
